dee8b430178b6462d2127ce2ec3d1f5bf300fd20
Matt Talluto <matthew.talluto@uqar.ca>
2014-11-21 17:42:48 -0500

    Finished the four-state reshaping

--- reshape/build_four_state_dataset.py
+++ reshape/build_four_state_dataset.py
@@ -13,38 +13,33 @@ plots = trans.read_plots(plotFile)
 nPlt = len(plots.keys())
 print("Read " + str(nPlt) + " plots.")
 
-trans.read_trees(treeFile, plots)
-print("Finished reading tree data")
-
 trans.read_climate(climFile, plots)
 print("Finished reading climate data")
 
-print("Checking for missing climate...")
-# check that every sample has climate
-totalSamples = 0
-missingClimate = 0
-for id in plots:
-    plot = plots[id]
-    for year in plot.samples:
-        samp = plot.samples[year]
-        totalSamples += 1
-        if not samp.hasClimate:
-            print("Missing climate for plot " + str(id) + " in year " + str(year))
-            missingClimate += 1
-print("Of " + str(totalSamples) + " samples, there are " + str(missingClimate) + " missing climate records")
+trans.read_trees(treeFile, plots)
+print("Finished reading tree data")
     
 # now print information
 trans.print_plot_summary(plots)
 trans.print_trans_table(plots)
 
 # output all transition & sample info
-print("Writing transitions to disk...")
 n = 1
 dataSources = ("transitions", "samples")
 method = "string"
 for data, outFile in zip(dataSources, (outputFileTr, outputFileSt)):
+    print("Writing " + data + " to disk...")
     with open(outFile, 'w') as of:
-        print(plots.values()[0].get_data(data, method, header=True), file=of)
+        # find the first plot with a valid header
+        hdr = ""
+        i = 0
+        while not hdr:
+            try:
+                hdr = plots.values()[i].get_data(data, method, header=True)
+            except IndexError:
+                i = i+1
+        print(hdr, file=of)
+        # print data for each plot
         for id in plots:
             pl = plots[id]
             out = pl.get_data(data, method)
@@ -52,16 +47,3 @@ for data, outFile in zip(dataSources, (outputFileTr, outputFileSt)):
                 print(l, file=of)
             n += len(out)
     print("Wrote " + str(n) + " lines to " + outFile)
-# 
-# # output all state info
-# print("Writing states to disk")
-# n = 1
-# with open(outputFileSt, 'w') as of:
-#     print(plots[plots.keys()[0]].output_states(header=True)[0], file=of)
-#     for id in plots:
-#         pl = plots[id]
-#         out = pl.output_states()
-#         for l in out:
-#             print(l, file=of)
-#         n += len(out)
-# print("Wrote " + str(n) + " lines")
