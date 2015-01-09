be71ee79154bb91150aa8c7dd0c67aee1e9adc95
SteveViss <s.vissault@yahoo.fr>
2015-01-09 15:50:00 -0500

    Add new climatic query and desc

--- src_sql/stm_plot_ids.sql
+++ src_sql/stm_plot_ids.sql
@@ -5,6 +5,11 @@
 -- Plots should be measured at least 2 times
 
 -- SQL Instructions dropping and rebuilding the view based on the criteria:
+	-- 1. Plots not temporary
+	-- 2. Plots location superior than -97.0 of longitude
+	-- 3. Plots size known
+
+	-- The second queries get the climate of all plots for 6 different climatic variables (interception with climatic rasters, 2010-1960)
 
 DROP MATERIALIZED VIEW IF EXISTS rdb_quicc.stm_plots_id CASCADE;
 CREATE MATERIALIZED VIEW rdb_quicc.stm_plots_id AS (
@@ -26,16 +31,16 @@ DROP MATERIALIZED VIEW IF EXISTS rdb_quicc.stm_plots_clim CASCADE;
 CREATE MATERIALIZED VIEW rdb_quicc.stm_plots_clim AS (
 SELECT plot_id, biovar, year_clim, ST_Value(rast,coord_postgis,false) AS val
 FROM
-(SELECT DISTINCT plot_id, coord_postgis
+(SELECT DISTINCT plot_id, ST_Transform(coord_postgis,4269) as coord_postgis
 FROM rdb_quicc.stm_plots_id) As plot_points,
-(SELECT ST_Union(ST_Transform(rast,4326)) AS rast, biovar, year_clim
+(SELECT rast, biovar, year_clim
 	FROM clim_rs.clim_allbiovars
 	WHERE biovar IN ('annual_mean_temp',
 		'pp_seasonality',
 		'pp_warmest_quarter',
 		'mean_diurnal_range',
 		'tot_annual_pp',
-		'mean_temperature_wettest_quarter')
-	AND year_clim >= 1960
-	GROUP BY biovar, year_clim) AS clim_rasters
+		'mean_temperature_wettest_quarter')) AS clim_rasters
+WHERE year_clim >=1960
+AND ST_Intersects(rast,coord_postgis)
 );
