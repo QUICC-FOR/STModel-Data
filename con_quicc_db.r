b94c5388d56bece6dea98b8d27386d2e304823bb
Matt Talluto <matthew.talluto@uqar.ca>
2015-01-14 15:34:28 -0500

    Minor changes to improve the scripts from off campus

--- con_quicc_db.r
+++ con_quicc_db.r
@@ -4,9 +4,16 @@
 #install.packages("RPostgreSQL")
 require("RPostgreSQL")
 
+## use when on campus
+# dbname <- "db_quicc_for"
+# dbhost <- "srbd04.uqar.ca"
+# dbport <- 5432
+# 
+
+# use when off campus
 dbname <- "db_quicc_for"
-dbhost <- "srbd04.uqar.ca"
-dbport <- 5432
+dbhost <- "127.0.0.1"
+dbport <- 55432
 
 drv <- dbDriver("PostgreSQL")
 source("credentials.r")
