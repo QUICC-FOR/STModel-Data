f7ceb71ad2d0d6a32792326e9dc6fef12acc3249
SteveViss <s.vissault@yahoo.fr>
2015-02-10 17:06:49 -0500

    Minor fix

--- install_dependencies.r
+++ install_dependencies.r
@@ -1,6 +1,6 @@
 #!/usr/bin/Rscript
-pckList = c("reshape2", "argparse", "RPostgreSQL", "maptools", "raster", "sp", "rgdal", 
-		"ggmap")
+pckList = c("reshape2", "argparse", "RPostgreSQL", "maptools", "raster", "sp", "rgdal",
+		"ggmap","ggplot2","rgeos")
 
-install.packages(pckList, repos="http://cran.utstat.utoronto.ca/", dependencies=TRUE, 
+install.packages(pckList, repos="http://cran.utstat.utoronto.ca/", dependencies=TRUE,
 		type="source")
