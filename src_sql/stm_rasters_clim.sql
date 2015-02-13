28ad848eebb85d142caea3e6b823d246514a3758
SteveViss <s.vissault@yahoo.fr>
2015-02-12 21:52:52 -0500

    Optimization query

--- src_sql/stm_rasters_clim.sql
+++ src_sql/stm_rasters_clim.sql
@@ -3,11 +3,14 @@
 
 DROP MATERIALIZED VIEW IF EXISTS rdb_quicc.stm_rasters_clim;
 CREATE MATERIALIZED VIEW rdb_quicc.stm_rasters_clim AS (
+SELECT year_measured, biovar, ST_Union(rast,'MEAN') AS rast FROM (
     SELECT rast, biovar, year_clim, year_measured
     FROM clim_rs.clim_allbiovars,
     (SELECT DISTINCT year_measured FROM rdb_quicc.stm_plots_id
     ORDER BY year_measured) as all_year_meas,
-    (SELECT ST_Transform(ST_ConvexHull(coord_postgis),4269) as ext FROM rdb_quicc.stm_plots_id) as coords_box
+    (SELECT DISTINCT ST_Transform(coord_postgis,4269) as coords FROM rdb_quicc.stm_plots_id) as coords_box
     WHERE year_clim <= year_measured AND year_clim > year_measured-15
-    AND ST_Intersects(rast,ext)
+    AND ST_Intersects(rast,coords)
+) AS all_rast
+GROUP BY year_measured, biovar
 );
