97283bf8f9871332e10e8f6a38a75b9dcc7bd91e
SteveViss <s.vissault@yahoo.fr>
2015-02-12 21:13:52 -0500

    Add query to store the 15 past years in rasters

--- src_sql/stm_plots_clim.sql
+++ src_sql/stm_plots_clim.sql
@@ -2,12 +2,12 @@
 
 DROP MATERIALIZED VIEW IF EXISTS rdb_quicc.stm_plots_clim;
 CREATE MATERIALIZED VIEW rdb_quicc.stm_plots_clim AS (
-SELECT plot_id, biovar, year_measured, year_clim, ST_Value(rast,coord_postgis,false) AS val
-FROM
-(SELECT plot_id, ST_Transform(coord_postgis,4269) as coord_postgis, year_measured
-FROM rdb_quicc.stm_plots_id) As plot_points,
-(SELECT rast, biovar, year_clim
-	FROM clim_rs.clim_allbiovars) AS clim_rasters
-WHERE year_clim <= year_measured AND year_clim > year_measured-15
-AND ST_Intersects(rast,coord_postgis)
+    SELECT plot_id, biovar, year_measured, year_clim, ST_Value(rast,coord_postgis,false) AS val
+    FROM
+    (SELECT plot_id, ST_Transform(coord_postgis,4269) as coord_postgis, year_measured
+    FROM rdb_quicc.stm_plots_id) As plot_points,
+    (SELECT rast, biovar, year_clim
+    	FROM clim_rs.clim_allbiovars) AS clim_rasters
+    WHERE year_clim <= year_measured AND year_clim > year_measured-15
+    AND ST_Intersects(rast,coord_postgis)
 );
