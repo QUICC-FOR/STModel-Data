dfce3c7d57fc6662d9c43be09940ab24179d9243
SteveViss <s.vissault@yahoo.fr>
2015-02-17 16:48:43 -0500

    Fix major issue with clim_query

--- src_sql/stm_plots_clim.sql
+++ src_sql/stm_plots_clim.sql
@@ -4,10 +4,13 @@ DROP MATERIALIZED VIEW IF EXISTS rdb_quicc.stm_plots_clim;
 CREATE MATERIALIZED VIEW rdb_quicc.stm_plots_clim AS (
     SELECT plot_id, biovar, year_measured, year_clim, ST_Value(rast,coord_postgis,false) AS val
     FROM
-    (SELECT plot_id, ST_Transform(coord_postgis,4269) as coord_postgis, year_measured
+    (SELECT ST_ConvexHull(ST_Collect(ST_Transform(coord_postgis,4269))) as bbox
+    FROM rdb_quicc.stm_plots_id) As env,
+    (SELECT plot_id, year_measured,
+        ST_Transform(coord_postgis,4269) as coord_postgis
     FROM rdb_quicc.stm_plots_id) As plot_points,
     (SELECT rast, biovar, year_clim
-    	FROM clim_rs.clim_allbiovars) AS clim_rasters
+        FROM clim_rs.clim_allbiovars) AS clim_rasters
     WHERE year_clim <= year_measured AND year_clim > year_measured-15
-    AND ST_Intersects(rast,coord_postgis)
+    AND ST_Intersects(rast,bbox)
 );
