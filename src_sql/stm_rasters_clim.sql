9049b5da71fb0b015a541a5d3768f21ce6cdc92c
SteveViss <s.vissault@yahoo.fr>
2015-02-17 16:48:43 -0500

    Fix major issue with clim_query

--- src_sql/stm_rasters_clim.sql
+++ src_sql/stm_rasters_clim.sql
@@ -6,11 +6,10 @@ CREATE MATERIALIZED VIEW rdb_quicc.stm_rasters_clim AS (
 SELECT year_measured, biovar, ST_Union(rast,'MEAN') AS rast FROM (
     SELECT rast, biovar, year_clim, year_measured
     FROM clim_rs.clim_allbiovars,
-    (SELECT DISTINCT year_measured FROM rdb_quicc.stm_plots_id
-    ORDER BY year_measured) as all_year_meas,
-    (SELECT DISTINCT ST_Transform(coord_postgis,4269) as coords FROM rdb_quicc.stm_plots_id) as coords_box
-    WHERE year_clim <= year_measured AND year_clim > year_measured-15
-    AND ST_Intersects(rast,coords)
+    (SELECT ST_ConvexHull(ST_Collect(ST_Transform(coord_postgis,4269))) as convex FROM rdb_quicc.stm_plots_id) as bbox,
+    (SELECT DISTINCT year_measured FROM rdb_quicc.stm_plots_id ORDER BY year_measured) as all_year_meas
+    WHERE ST_Intersects(rast,convex)
+    AND year_clim <= year_measured AND year_clim > year_measured-15
 ) AS all_rast
 GROUP BY year_measured, biovar
 );
