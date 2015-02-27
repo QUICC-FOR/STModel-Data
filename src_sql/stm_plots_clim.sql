c10233c4b1101726f4f9446c964ffc8060f2dce0
SteveViss <s.vissault@yahoo.fr>
2015-02-27 14:00:40 -0500

    Including indexes on MVs

--- src_sql/stm_plots_clim.sql
+++ src_sql/stm_plots_clim.sql
@@ -1,16 +1,15 @@
 -- MV on climate
 
-DROP MATERIALIZED VIEW IF EXISTS rdb_quicc.stm_plots_clim;
+DROP MATERIALIZED VIEW IF EXISTS rdb_quicc.stm_plots_clim CASCADE;
 CREATE MATERIALIZED VIEW rdb_quicc.stm_plots_clim AS (
-    SELECT plot_id, biovar, year_measured, year_clim, ST_Value(rast,coord_postgis,false) AS val
-    FROM
-    (SELECT ST_ConvexHull(ST_Collect(ST_Transform(coord_postgis,4269))) as bbox
-    FROM rdb_quicc.stm_plots_id) As env,
-    (SELECT plot_id, year_measured,
-        ST_Transform(coord_postgis,4269) as coord_postgis
-    FROM rdb_quicc.stm_plots_id) As plot_points,
-    (SELECT rast, biovar, year_clim
-        FROM clim_rs.clim_allbiovars) AS clim_rasters
-    WHERE year_clim <= year_measured AND year_clim > year_measured-15
-    AND ST_Intersects(rast,bbox)
+     SELECT plot_id, biovar, year_measured, year_clim, ST_Value(rast,coord_postgis,false) AS val
+     FROM rdb_quicc.stm_plots_id
+     LEFT JOIN rdb_quicc.stm_rasters_clim USING (year_measured)
+     WHERE ST_Intersects(rast,coord_postgis)
+     ORDER BY plot_id, year_measured, year_clim
 );
+
+CREATE INDEX stm_mv_clim_rs_search
+  ON rdb_quicc.stm_plots_clim AS
+  USING btree
+  (year_measured ,biovar);
