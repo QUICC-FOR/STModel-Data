40fd2483cb47d634a8d026ea534ea58a2a76f6bf
SteveViss <s.vissault@yahoo.fr>
2015-03-02 17:43:39 -0500

    Increase signif rasters queries perf

--- src_sql/stm_plots_clim.sql
+++ src_sql/stm_plots_clim.sql
@@ -2,11 +2,10 @@
 
 DROP MATERIALIZED VIEW IF EXISTS rdb_quicc.stm_plots_clim CASCADE;
 CREATE MATERIALIZED VIEW rdb_quicc.stm_plots_clim AS (
-     SELECT plot_id, biovar, year_measured, year_clim, ST_Value(rast,coord_postgis,false) AS val
+     SELECT plot_id, biovar, year_measured, ST_Value(rast_climato,coord_postgis,false) AS val
      FROM rdb_quicc.stm_plots_id
-     LEFT JOIN rdb_quicc.stm_rasters_clim USING (year_measured)
-     WHERE ST_Intersects(rast,coord_postgis)
-     ORDER BY plot_id, year_measured, year_clim
+     LEFT JOIN rdb_quicc.stm_rasters_climato USING (year_measured)
+     WHERE ST_Intersects(rast_climato,coord_postgis)
 );
 
 CREATE INDEX stm_mv_clim_rs_search
--- src_sql/stm_rasters_clim.sql
+++ src_sql/stm_rasters_clim.sql
@@ -1,21 +1,23 @@
 -- Stored past-climate in rasters for each year measured
 -- Each raster contains the climatic average (15 past-years).
 
-DROP MATERIALIZED VIEW IF EXISTS rdb_quicc.stm_rasters_clim CASCADE;
-CREATE MATERIALIZED VIEW rdb_quicc.stm_rasters_clim AS (
+DROP MATERIALIZED VIEW IF EXISTS rdb_quicc.stm_rasters_climato CASCADE;
+CREATE MATERIALIZED VIEW rdb_quicc.stm_rasters_climato AS (
+SELECT biovar, year_measured, ST_UNION(rast,'MEAN') AS rast_climato FROM (
  SELECT biovar, year_clim, year_measured, rast
     FROM clim_rs.clim_allbiovars,
     (SELECT ST_ConvexHull(ST_Collect(coord_postgis)) as convex, year_measured FROM rdb_quicc.stm_plots_id GROUP BY year_measured) as bbox
     WHERE ST_Intersects(rast,convex)
-    AND year_clim <= year_measured AND year_clim > year_measured-15
+    AND year_clim <= year_measured AND year_clim > year_measured-15) AS all_rast_last_15yrs
+GROUP BY biovar, year_measured
 );
 
 CREATE INDEX stm_mv_clim_rs_st_convexhull_idx
   ON rdb_quicc.stm_rasters_clim
   USING gist
-  (st_convexhull(rast));
+  (st_convexhull(rast_climato));
 
 CREATE INDEX stm_mv_clim_rs_search
   ON rdb_quicc.stm_rasters_clim
   USING btree
-  (year_clim, biovar, year_measured);
+  (biovar, year_measured);
