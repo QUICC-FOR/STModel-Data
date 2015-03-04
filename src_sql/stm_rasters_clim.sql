e03aea471f8a03c42111844786bf32bf087ac836
SteveViss <s.vissault@yahoo.fr>
2015-03-03 22:48:58 -0500

    Tiled climato rasters

--- src_sql/stm_rasters_clim.sql
+++ src_sql/stm_rasters_clim.sql
@@ -3,7 +3,7 @@
 
 DROP MATERIALIZED VIEW IF EXISTS rdb_quicc.stm_rasters_climato CASCADE;
 CREATE MATERIALIZED VIEW rdb_quicc.stm_rasters_climato AS (
-SELECT biovar, year_measured, ST_UNION(rast,'MEAN') AS rast_climato FROM (
+SELECT biovar, year_measured, ST_Tile(ST_Union(rast,'MEAN'),10,10) AS rast_climato FROM (
  SELECT biovar, year_clim, year_measured, rast
     FROM clim_rs.clim_allbiovars,
     (SELECT ST_ConvexHull(ST_Collect(coord_postgis)) as convex, year_measured FROM rdb_quicc.stm_plots_id GROUP BY year_measured) as bbox
@@ -13,11 +13,11 @@ GROUP BY biovar, year_measured
 );
 
 CREATE INDEX stm_mv_clim_rs_st_convexhull_idx
-  ON rdb_quicc.stm_rasters_clim
+  ON rdb_quicc.stm_rasters_climato
   USING gist
   (st_convexhull(rast_climato));
 
 CREATE INDEX stm_mv_clim_rs_search
-  ON rdb_quicc.stm_rasters_clim
+  ON rdb_quicc.stm_rasters_climato
   USING btree
   (biovar, year_measured);
