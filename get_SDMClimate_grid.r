dbe889e470dff425d43dc38decfdadfca4d1e83f
SteveViss <s.vissault@yahoo.fr>
2015-03-18 20:20:12 -0400

    Fix prob with NA

--- get_SDMClimate_grid.r
+++ get_SDMClimate_grid.r
@@ -10,20 +10,12 @@
 ## Variable selected for the SDM (see 0-SDM_explo_vars in STModel-Calibration model)
 ## ---------------------------------------------
 
-# 6 climatic variables selected to build the SDM
-# - 'annual_mean_temp' (WARNING: Need to be divided by 10 to get decimal)
-# - 'min_temp_coldest_mont'
-# - 'mean_temp_warmest_quarter'
-# - 'mean_temp_coldest_quarter'
-# - 'annual_pp'
-# - 'pp_seasonality'
-
 ## Get grid from quicc-for database
 ## ---------------------------------------------
 
 # Database connection
 source('./con_quicc_db.r')
-#source('./con_quicc_db_local.r')
+source('./con_quicc_db_local.r')
 
 #Load librairies
 require('reshape2')
@@ -34,21 +26,7 @@ query_SDMClimate_grid  <- "SELECT ST_X(geom) as lon, ST_Y(geom) as lat, val, bio
     SELECT biovar, ST_Union(ST_Clip(ST_Resample(rast,ref_rast),env_stm.env_plots),'MEAN') as rasters
     FROM
     (SELECT rast, biovar,year_clim FROM clim_rs.past_clim_allbiovars
-     WHERE (year_clim >= 1970 AND year_clim <= 2000)
-    AND biovar IN (
-
-    -- SDM selected variables
-    -------------------------------
-
-    'annual_mean_temp',
-    'tot_annual_pp',
-    'mean_diurnal_range',
-    'pp_warmest_quarter',
-    'pp_wettest_period',
-    'mean_temp_wettest_quarter',
-    'mean_temp_driest_quarter'
-
-    )) AS rast_noram,
+     WHERE year_clim >= 1901 AND year_clim <= 1930) AS rast_noram,
     (SELECT ST_Transform(ST_ConvexHull(ST_Collect(stm_plots_id.coord_postgis)),4269) as env_plots FROM rdb_quicc.stm_plots_id) AS env_stm,
     (SELECT rast as ref_rast FROM clim_rs.past_clim_allbiovars WHERE biovar = 'annual_mean_temp' LIMIT 1) as ref
     WHERE ST_Intersects(rast_noram.rast,env_stm.env_plots)
@@ -68,13 +46,16 @@ SDMClimate_grid = res_SDMClimate_grid
 SDMClimate_grid$biovar <- as.factor(SDMClimate_grid$biovar)
 SDMClimate_grid <- dcast(SDMClimate_grid,lon+lat ~ biovar, value.var="val")
 
-#Delete NA
-SDMClimate_grid <- SDMClimate_grid[!is.na(SDMClimate_grid[,c(3:ncol(SDMClimate_grid))]),]
-
 #Conversion unit
 SDMClimate_grid$mean_diurnal_range <- SDMClimate_grid$mean_diurnal_range/10
 SDMClimate_grid$mean_temp_wettest_quarter <- SDMClimate_grid$mean_temp_wettest_quarter/10
 SDMClimate_grid$mean_temp_driest_quarter <- SDMClimate_grid$mean_temp_driest_quarter/10
+SDMClimate_grid$max_temp_warmest_period <- SDMClimate_grid$max_temp_warmest_period/10
+SDMClimate_grid$mean_temp_coldest_quarter <- SDMClimate_grid$mean_temp_coldest_quarter/10
+SDMClimate_grid$mean_temp_warmest_quarter <- SDMClimate_grid$mean_temp_warmest_quarter/10
+SDMClimate_grid$min_temp_coldest_period <- SDMClimate_grid$min_temp_coldest_period/10
+SDMClimate_grid$temp_annual_range <- SDMClimate_grid$temp_annual_range/10
+SDMClimate_grid$temp_seasonality <- SDMClimate_grid$temp_seasonality/100
 
 ## Write
 write.table(SDMClimate_grid, file="out_files/SDMClimate_grid.csv", sep=',', row.names=FALSE)
