1f459d98055da3f4563a555d3b1afe91e2c93f6c
SteveViss <s.vissault@yahoo.fr>
2015-03-31 18:32:43 -0400

    Retrieve columns from soil MV

--- src_sql/stm_plots_soil_slp.sql
+++ src_sql/stm_plots_soil_slp.sql
@@ -1,6 +1,6 @@
 DROP MATERIALIZED VIEW rdb_quicc.stm_plots_soil_slp; 
 CREATE MATERIALIZED VIEW rdb_quicc.stm_plots_soil_slp AS(
-    SELECT plot_id,coord, ST_Value(demelevation.rast::raster,coord,false) as elv,
+    SELECT plot_id,
     ST_Value(ST_Slope(demelevation.rast::raster,1,'32BF'::text,'DEGREES'::text,111120.0::double precision,false),coord,false)::float as slp,
     ST_Value(soil.rast,coord,false) as soil FROM 
     map_world.demelevation,
