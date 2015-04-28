17dc31171643cbfbf2f1956582a6ab9b535a9c9b
SteveViss <s.vissault@yahoo.fr>
2015-04-27 22:10:11 -0400

    Add colosse con_quicc_db.r & Add fut_climData target on makefile

--- get_futureClim.r
+++ get_futureClim.r
@@ -15,9 +15,14 @@ require('reshape2')
 GCM_df <- read.csv("./list_GCMs.csv")
 GCM_df <- subset(GCM_df, scenario == 'rcp60')
 
+windows <- seq(2000,2095,5)  
+out_folder <- "./out_files/fut_clim/"
 
-SELECT ST_X(geom) as lon, ST_Y(geom) as lat, x , y, var, yr, val FROM (
-SELECT var,yr,(ST_PixelAsCentroids(ST_Clip(raster,1,env_plots,true),1,true)).*
+
+query <- paste("SELECT ST_X(geom) as lon, ST_Y(geom) as lat, x , y, var, ", windows[i] ," as min_yr,",windows[i+1]," as max_yr, val FROM (
+SELECT var,yr,(ST_PixelAsCentroids(ST_Union(ST_Clip(raster,1,env_plots,true),'MEAN'),1,true)).*
 FROM clim_rs.fut_clim_biovars,
-(SELECT ST_Polygon(ST_GeomFromText('LINESTRING(-79.95454 43.04572,-79.95454 50.95411,-60.04625 50.95411,-60.04625 43.04572,-79.95454 43.04572)'), 4326) as env_plots FROM rdb_quicc.stm_plots_id) as envelope
-WHERE (var='bio1' OR var='bio12') AND yr=2000 AND clim_center='NASA_GISS' AND mod='GISS_E2_R' AND run='r1i1p1_historical'  AND scenario='rcp85' AND ST_Intersects(raster,env_plots)) as pixels;
+(SELECT ST_Polygon(ST_GeomFromText('LINESTRING(-79.95454 43.04572,-79.95454 50.95411,-60.04625 50.95411,-60.04625 43.04572,-79.95454 43.04572)'), 4326) as env_plots) as envelope
+WHERE (var='bio1' OR var='bio12') AND (yr>=",windows[i]," AND yr <=",windows[i+1],") AND clim_center='",GCM_df[x,1],"' AND mod='",GCM_df[x,2],"' AND run='",GCM_df[x,3],"'  AND scenario='",GCM_df[x,4],"' AND ST_Intersects(raster,env_plots)
+GROUP BY var, yr
+) as pixels;",sep="")
