0812f02a63f6e88577bf3300a9500374b854848d
SteveViss <s.vissault@yahoo.fr>
2015-06-11 16:25:27 -0400

    Change study area for shp cropping; simplify countries

--- get_studyAreaShapeFiles.r
+++ get_studyAreaShapeFiles.r
@@ -7,14 +7,15 @@
 
 # Database connection
 
-#source('./con_quicc_db.r')
-source('./con_quicc_db_local.r')
+source('./con_quicc_db.r')
+#source('./con_quicc_db_local.r')
 source('./Rpostgis.r') #Extract spatial layer form the DB
 
 # Library
-require(ggplot2)
-require(raster)
-require(sp)
+library(ggplot2)
+library(raster)
+library(sp)
+library(rgeos)
 
 # Extract shapefiles from the database
 countries <- dbReadSpatial(con, schemaname="temp_quicc", tablename="stm_countries_shapes", geomcol="geom")
@@ -30,17 +31,17 @@ writeOGR(great_lakes, "./out_files/shapefiles/", "great_lakes_stm_area", driver=
 
 
 #Convert to ggplot2 format
-ext_geo <- extent(c(-79.95454,-60.04625,43.04572,50.95411))
+ext_geo <- extent(c(-79.87535,-60.12543,43.12493,50.8749 ))
 #ext_geo <- extent(c(-96.98904, -57.30813,  35.25441,  52.90010))
 
 
-#Crop and fortify
+#Crop, simplify and fortify
 lakes <- crop(lakes,ext_geo)
 great_lakes <- crop(great_lakes,ext_geo)
-countries <- crop(countries,ext_geo)
+countries <- gSimplify(crop(countries,ext_geo),0.005)
 df.lakes <- fortify(lakes)
 df.countries <- fortify(countries)
 df.great_lakes <- fortify(great_lakes)
 
 #Save ggplot2 format as an R object
-save(df.lakes,df.countries,df.great_lakes,file="./out_files/shp_stm_area.rdata")
+save(lakes,great_lakes,countries,df.lakes,df.countries,df.great_lakes,file="./out_files/shp_stm_area.rdata")
