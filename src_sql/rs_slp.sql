c513e68461624a1dea31a274d14c9639fffda74b
SteveViss <s.vissault@yahoo.fr>
2015-06-22 16:53:01 -0400

    Declare type var

--- src_sql/rs_slp.sql
+++ src_sql/rs_slp.sql
@@ -1,7 +1,7 @@
 CREATE MATERIALIZED VIEW rdb_quicc.rs_slp AS (
     SELECT var,  ST_Tile(rast,10,10) AS rast FROM (
 
-             SELECT 'slp' AS var, ST_Transform(ST_Slope(ST_Union(demelevation.rast::raster),1,'32BF'::text,'DEGREES'::text,111120.0::double precision,false),4269) as rast
+             SELECT 'slp' ::varchar(10) AS var, ST_Transform(ST_Slope(ST_Union(demelevation.rast::raster),1,'32BF'::text,'DEGREES'::text,111120.0::double precision,false),4269) as rast
              FROM map_world.demelevation,
                   (SELECT ST_Transform(ST_GeomFromText('POLYGON((-79.95454 43.04572,-79.95454 50.95411,-60.04625 50.95411,-60.04625 43.04572,-79.95454 43.04572))',4326),4269) as env_plots) AS env_stm
               WHERE ST_Intersects(ST_Transform(rast,4269),env_stm.env_plots)
