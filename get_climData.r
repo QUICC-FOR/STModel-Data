bda8d94c266355e97434e94a8c06aa73c5d7f27b
SteveViss <s.vissault@yahoo.fr>
2015-06-22 16:10:11 -0400

    manage NA in climData

--- get_climData.r
+++ get_climData.r
@@ -75,7 +75,10 @@ climData_reshape <- dcast(climData_reshape,plot_id+year_measured ~ biovar, value
 
 ##Add soil
 climData_reshape <- left_join(climData_reshape,soilData,by="plot_id")
-
+climData_reshape[which(climData_reshape$ph_2cm==255), "ph_2cm"] <- NA 
+climData_reshape[which(climData_reshape$ph_10cm==255), "ph_10cm"] <- NA 
+climData_reshape[which(climData_reshape$ph_10cm==255), "soil"] <- NA 
+climData_reshape[which(climData_reshape$slp==-9999), "slp"] <- NA 
 
 ## Scale transformation from NRCAN
 ## NOTE: For bio grids the values of temperature must be divided by 10, and the values of Temperature Seasonality (C of V) must be divided by 100
