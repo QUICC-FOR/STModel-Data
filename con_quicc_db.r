f1b45dc036a480e89827f5a89fc9bf5f76c6ff37
Steve Vissault <sviss@ip33.m>
2016-10-05 10:31:34 -0400

    modif on con_quicc_db

--- con_quicc_db.r
+++ con_quicc_db.r
@@ -1,8 +1,11 @@
 require("RPostgreSQL")
 
-drv <- dbDriver("PostgreSQL")
+## use when logged to ip33 with your regular account
 dbname <- "quicc_for_dev"
 dbhost <- "localhost"
-user <- "postgres"
+dbuser <- 'sviss' # please change for your username, if you don't have one ask to Steve
+dbport <- 5432
+drv <- dbDriver("PostgreSQL")
 
-con <- dbConnect(drv, dbname=dbname,host=dbhost,user=user)
+con <- dbConnect(drv, host=dbhost, port=dbport, dbname=dbname,
+                 user=dbuser)
